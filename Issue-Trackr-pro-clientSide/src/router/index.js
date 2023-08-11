76527e4f022610dcf6de8a6f50c5a93041e2fd69
mohamed haddad <m.haddad@sysbas.com>
2023-08-11 16:06:24 +0100

    Add client-side directory and initilise the Front-End workspace

---
+++ Issue-Trackr-pro-clientSide/src/router/index.js
@@ -0,0 +1,14 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomePage from '../views/HomePageVue.vue'
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomePage
+    }
+  ]
+})
+
+export default router
